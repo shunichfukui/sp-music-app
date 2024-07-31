c16079801f5375cd9a9d0eae69779d32a29cea14
shunich <fukusyun3@gmail.com>
2024-07-31 15:45:55 +0900

    feat: tailwindcss setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css';
-
-function App() {
-  return <div className="App">テスト</div>;
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      <main className="flex-1 p-8 mb-20">
+        <header className="flex justify-between items-center mb-10">
+          <h1 className="text-4xl font-bold">Music App</h1>
+        </header>
+        <section>
+          <h2 className="text-2xl font-semibold mb-5">あなたにオススメの音楽</h2>
+        </section>
+      </main>
+    </div>
+  );
 }
-
-export default App;
